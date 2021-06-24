70201b9e51db62f0944b41c8612a1af886532a21
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-06-24 18:25:22 -0300

    Cria o chat e o insere as mensagens no BD

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import Chat from './Chat'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 function App() {
   return (
     <div className="App">
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Client from './Api'
 import {
-  ApolloClient,
-  InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  useMutation,
+  gql,
 } from "@apollo/client";
+import {Container, Row, Col, FormInput,Button} from  'shards-react'
 
 const GET_MESSAGES = gql`
   query {
@@ -16,25 +17,119 @@ const GET_MESSAGES = gql`
       createdAt
     }
   }`;
+
+const POST_MESSAGES = gql`
+mutation ($user:String!, $content: String!){
+	createChat( user: $user, content: $content){
+    content
+  }
+}`;
 const Messages = ({user})=>{
   const { data } = useQuery(GET_MESSAGES);
   if(!data){
     return "no value"
   }
-    return JSON.stringify(data)
+
+    return (
+      <>
+      {data.chats.map(({id, user: messageUser, content, createdAt})=>(
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height:50,
+                width:50,
+                marginRight:"0.5em",
+                border: "2px solid #e5e6ea",
+                borderRadius:25,
+                textAlign:"center",
+                fontSize:'18px',
+                paddingTop:8,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          
+          <div
+            style={{
+              background: user === messageUser ? "green" : "lightgray",
+              color: user === messageUser ? "white" : "black",
+              padding:"1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+      </>
+    )
 }
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
-});
 
 const Chat = () => {
+  const [person, setPerson] = useState({
+    user:'Abc',
+    content:'',
+  })
+  const [postMessage] = useMutation(POST_MESSAGES);
+  const onSend =() =>{
+    if(person.content.length > 0){  
+       postMessage({
+         variables: person,
+       })
+     } 
+     setPerson({
+       ...person,
+       content:'',
+     })
+   }
   return (
-    <div> alo <Messages user="Abc" /></div>
-  )
-}
+    <Container> 
+      <Messages user={person.user} />
+      <Row>
+        <Col xs={2} style={{padding: 0}}>
+        <FormInput 
+          label="User"
+          value={person.user}
+          onChange={(event) => setPerson({
+            ...person,
+            user: event.target.value
+          })}
+        />
+        </Col>
+        <Col xs={8}>
+        <FormInput 
+          label="Content"
+          value={person.content}
+          onChange={(event) => setPerson({
+            ...person,
+            content: event.target.value
+          })}
+          onKeyUp={(event)=>{
+            if (event.keyCode === 13){
+              onSend();
+            }
+          }}
+        />
+        </Col>
+        <Col xs={2} style={{padding: 0}}>
+          <Button onClick={()=>onSend()}>
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  )}
  const Apollo = () => (
-  <ApolloProvider client={client}>
+  <ApolloProvider client={Client}>
     <Chat />
   </ApolloProvider>
 ) 
